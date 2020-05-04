9d0e0e176048e502d226917a9bac0a57473f0dc7
gusflopes <gusflopes86@gmail.com>
2020-05-04 17:15:02 -0400

    feat: Health Check Route

--- start/routes.ts
+++ start/routes.ts
@@ -19,9 +19,15 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/health', async ({response}) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
 })
 
 // Public Routes
